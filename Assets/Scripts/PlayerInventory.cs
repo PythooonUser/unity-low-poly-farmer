503c733b46f96f4e47743a0fac4b5fffbc5940de
Felix Siebeneicker <felix.sieben@t-online.de>
2018-12-30 13:36:53 +0100

    Adding player inventory

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Player Inventory")]
+public class PlayerInventory : ScriptableObject
+{
+    [SerializeField]
+    private int seedAmount;
+
+    public void SetDefaultState()
+    {
+        this.seedAmount = 4;
+    }
+
+    public int GetSeedAmount()
+    {
+        return this.seedAmount;
+    }
+}
