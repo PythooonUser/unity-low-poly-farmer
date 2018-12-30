4d57e4241c32b1be1cd1558c134d8e2c1cb53369
Felix Siebeneicker <felix.sieben@t-online.de>
2018-12-30 13:25:55 +0100

    Adding simple camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float interactionRange = 10f;
+    public LayerMask interactionLayermask;
+
+    new Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactionRange, interactionLayermask))
+        {
+            if (hit.collider.tag == "Field") { }
+        }
+    }
+}
