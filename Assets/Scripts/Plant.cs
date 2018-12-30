0985f211caad42159e09261f1e834860736178ff
Felix Siebeneicker <felix.sieben@t-online.de>
2018-12-30 15:08:11 +0100

    Adding grow mechanic to plants

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -4,8 +4,43 @@ using UnityEngine;
 
 public class Plant : MonoBehaviour
 {
+    public float growthTime = 4f;
     public int seedYield = 2;
     [HideInInspector] public PlantAnchor plantAnchor;
+    public GameObject fruitObjects;
+
+    new Collider collider;
+    bool canBeHarvested;
+    float startTime;
+
+    void Start()
+    {
+        this.collider = GetComponent<BoxCollider>();
+        this.collider.enabled = false;
+        fruitObjects.SetActive(false);
+        transform.localScale = Vector3.zero;
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (canBeHarvested) { return; }
+
+        float currentTime = Time.time - startTime;
+
+        if (currentTime <= growthTime)
+        {
+            float scale = currentTime / growthTime;
+            transform.localScale = Vector3.one * scale;
+        }
+
+        else
+        {
+            this.collider.enabled = true;
+            fruitObjects.SetActive(true);
+            canBeHarvested = true;
+        }
+    }
 
     public int Harvest()
     {
